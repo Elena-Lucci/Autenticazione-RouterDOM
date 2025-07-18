c4613f60a6be9767c8b54cd42700064ab35c559b
Carlo Ganci <carloganci98@gmail.com>
2025-07-18 10:56:02 +0200

    fixata pages registrazione

--- authentication-system-project/src/Context/AuthContext.jsx
+++ authentication-system-project/src/Context/AuthContext.jsx
@@ -3,50 +3,61 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
-export const useAuth = ()=> useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
 
-export function AuthProvider({children}) {
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
-    const [users, setUsers] = useState(JSON.parse(localStorage.getItem("users")) || []);
-    useEffect(() => {localStorage.setItem("users", JSON.stringify(users))}, [users]);
-    const [message, setMessage] = useState("");
-    const navigate = useNavigate();
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+  const [users, setUsers] = useState(
+    JSON.parse(localStorage.getItem("users")) || []
+  );
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-    function registrazione(formData){
-        setMessage("");
-        const userExists = users.find((x) => x.email === formData.email);
-        if(!userExists){
-            setMessage("Utente registrato con successo");
-            setUsers((prev) => [...prev, formData]);
-        } else {
-            setMessage("Utente già registrato");
-        }
+  function registrazione(formData) {
+    setMessage("");
+    const userExists = users.find((x) => x.email === formData.email);
+    if (!userExists) {
+      setMessage("Utente registrato con successo");
+      setUsers((prev) => [...prev, formData]);
+      navigate("/login");
+    } else {
+      setMessage("Utente già registrato");
     }
+  }
 
-    function login(email, password) {
-        setMessage("");
-        const userExists = users.find((x) => x. email === email && x.password === password);
-        if(!userExists) {
-            setMessage("Credenziali errate");
-        } else {
-                setUser(userExists);
-                localStorage.setItem("user", JSON.stringify(userExists));
-                navigate('/dashboard');
-            } 
-        
+  function login(email, password) {
+    setMessage("");
+    const userExists = users.find(
+      (x) => x.email === email && x.password === password
+    );
+    if (!userExists) {
+      setMessage("Credenziali errate");
+    } else {
+      setUser(userExists);
+      localStorage.setItem("user", JSON.stringify(userExists));
+      navigate("/dashboard");
     }
+  }
 
-    function logout() {
-        setMessage("");
-        setUser(null);
-        localStorage.removeItem("user");
-        setMessage("Logout effettuato con successo");
-    }
+  function logout() {
+    setMessage("");
+    setUser(null);
+    localStorage.removeItem("user");
+    setMessage("Logout effettuato con successo");
+  }
 
-return(
+  return (
     <>
-        <AuthContext.Provider value={{user, login, logout, registrazione, message, users }}>{children}</AuthContext.Provider>
+      <AuthContext.Provider
+        value={{ user, login, logout, registrazione, message, users }}
+      >
+        {children}
+      </AuthContext.Provider>
     </>
-)
+  );
 }
-
