0b82fbfcd47f0f6603bc57b57c47916e5507c836
francesca.castangia <castangiaf2@gmail.com>
2025-07-18 12:11:53 +0200

    implementato stile tailwind

--- authentication-system-project/src/Pages/Login.jsx
+++ authentication-system-project/src/Pages/Login.jsx
@@ -18,15 +18,15 @@ function Login(){
 
 
     return(
-       <div>
-        <form onSubmit={handleSubmit}>
-            <label>Email</label>
-            <input type="email" value={email} onChange={(e)=> setEmail(e.target.value) } placeholder="Inserisci la tua email" required />
+       <div className="shadow-lg p-4 rounded-lg">
+        <form className="flex flex-col gap-[20px] justify-center " onSubmit={handleSubmit}>
+            <label className="text-left">Email</label>
+            <input className={email?" shadow-lg rounded-xl p-1.5":"shadow-lg rounded-xl p-1.5 shadow-red-500"} type="email" value={email} onChange={(e)=> setEmail(e.target.value) } placeholder="Inserisci la tua email" required />
 
-            <label>Password</label>
-            <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Inserisci la tua password" required />
+            <label className="text-left">Password</label>
+            <input  className="shadow-lg rounded-xl p-1.5" type="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Inserisci la tua password" required />
 
-            <button type="submit">Login</button>
+            <button className="py-2 px-4 bg-[#016450] text-[var(--bg-color)] rounded-xl" type="submit">Login</button>
             <p>Non sei registrato? <Link to={'/registrazione'}>Registrati</Link> </p>
             {message && <p>{message}</p>}
         </form>
